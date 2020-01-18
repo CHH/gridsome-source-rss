3ac579b697a9378fc7ea35da83a0bbf431b90b30
Christoph Hochstrasser <christoph@hochstrasser.io>
2020-01-18 22:56:03 +0100

    Use 2 spaces indent

--- src/index.js
+++ src/index.js
@@ -2,40 +2,40 @@ const RssParser = require('rss-parser')
 const omit = require('lodash.omit')
 
 class RssFeedSource {
-    static defaultOptions() {
-        return {
-            typeName: 'FeedNode',
-            feedUrl: null,
-            parser: {}
-        }
+  static defaultOptions() {
+    return {
+      typeName: 'FeedNode',
+      feedUrl: null,
+      parser: {}
     }
-
-    constructor(api, options) {
-        this.api = api
-        this.options = options
-        this.rss = new RssParser(this.options.parser)
-
-        api.loadSource(async actions => {
-            await this.loadFeed(actions)
-        })
+  }
+  
+  constructor(api, options) {
+    this.api = api
+    this.options = options
+    this.rss = new RssParser(this.options.parser)
+    
+    api.loadSource(async actions => {
+      await this.loadFeed(actions)
+    })
+  }
+  
+  async loadFeed(actions) {
+    const collection = actions.addCollection({
+      typeName: this.options.typeName,
+    })
+    
+    const feed = await this.rss.parseURL(this.options.feedUrl)
+    const feedMeta = omit(feed, ['items'])
+    
+    for (const item of feed.items) {
+      collection.addNode({
+        id: item.guid,
+        feedMeta,
+        ...item,
+      })
     }
-
-    async loadFeed(actions) {
-        const collection = actions.addCollection({
-            typeName: this.options.typeName,
-        })
-
-        const feed = await this.rss.parseURL(this.options.feedUrl)
-        const feedMeta = omit(feed, ['items'])
-
-        for (const item of feed.items) {
-            collection.addNode({
-                id: item.guid,
-                feedMeta,
-                ...item,
-            })
-        }
-    }
+  }
 }
 
 module.exports = RssFeedSource
