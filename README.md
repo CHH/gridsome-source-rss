f14f504208094ca4d6d5e94aa0f3482a3f8d6a1b
Christoph Hochstrasser <christoph@hochstrasser.io>
2020-01-18 14:46:10 +0100

    Add feed meta to each item, add README

---
+++ README.md
@@ -0,0 +1,40 @@
+# gridsome-source-rss
+
+> Use an RSS feed as a source for gridsome
+
+## Install
+
+- `yarn add gridsome-source-rss`
+- `npm install gridsome-source-rss`
+
+## Usage
+
+```
+module.exports = {
+  plugins: [
+    {
+      use: "gridsome-source-rss",
+      options: {
+        feedUrl: "https://hnrss.org/frontpage",
+        typeName: 'HNFrontpageItem'
+        // Parser options, see: https://www.npmjs.com/package/rss-parser
+        parser: {}
+      }
+    },
+  ],
+}
+```
+
+## Options
+
+### feedUrl
+
+URL of the feed, passed directly to `parseURL`.
+
+### typeName
+
+Type name of the collection
+
+### parser
+
+Parser options, see the [`rss-parser` documentation](https://www.npmjs.com/package/rss-parser). Use this to set HTTP options, for example.
--- src/index.js
+++ src/index.js
@@ -1,17 +1,19 @@
 const RssParser = require('rss-parser')
-const parser = new RssParser()
+const omit = require('lodash.omit')
 
 class RssFeedSource {
     static defaultOptions() {
         return {
             typeName: 'FeedNode',
             feedUrl: null,
+            parser: {}
         }
     }
 
     constructor(api, options) {
         this.api = api
         this.options = options
+        this.rss = new RssParser(this.options.parser)
 
         api.loadSource(async actions => {
             await this.loadFeed(actions)
@@ -23,11 +25,13 @@ class RssFeedSource {
             typeName: this.options.typeName,
         })
 
-        const feed = await parser.parseURL(this.options.feedUrl)
+        const feed = await this.rss.parseURL(this.options.feedUrl)
+        const feedMeta = omit(feed, ['items'])
 
         for (const item of feed.items) {
             collection.addNode({
                 id: item.guid,
+                feedMeta,
                 ...item,
             })
         }
